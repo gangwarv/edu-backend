2b156ecf7af8ee24ac47a5d9ff7deb7d209dee36
Vishal Gangwar <vikash4654@gmail.com>
2020-04-03 22:12:33 +0530

    start cmd chng

--- models/student/student.js
+++ models/student/student.js
@@ -1,55 +1,60 @@
-const mongoose = require('mongoose');
-const { mobile, address } = require('../shared/shared-schema');
+const mongoose = require("mongoose");
+const { mobile, address } = require("../shared/shared-schema");
 
-const schema = new mongoose.Schema({
+const schema = new mongoose.Schema(
+  {
     session: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
+    enrollmentNo: String,
+    rollNo: String,
     firstName: {
-        type: String,
-        lowercase: true, 
-        trim: true,
-        required: true
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true
     },
     lastName: {
-        type: String,
-        lowercase: true, 
-        trim: true,
-        required: true
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true
     },
     gender: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     dateOfBirth: {
-        type: Date
+      type: Date
     },
     mobile: mobile,
     email: {
-        type: String,
-        lowercase: true,
-        required: true
+      type: String,
+      lowercase: true,
+      required: true
     },
     course: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-        ref: "Course"
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "Course"
     },
     lateralEntry: {
-        type: Boolean
+      type: Boolean
     },
     category: {
-        type: mongoose.Types.ObjectId,
-        required: true
+      type: mongoose.Types.ObjectId,
+      required: true
     },
     paddress: address,
     caddress: address,
     nationality: {
-        type: String,
-        required: true,
-        default: 'indian'
-    },
-}, { timestamps: true });
+      type: String,
+      required: true,
+      default: "indian"
+    }
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Student', schema);
+module.exports = mongoose.model("Student", schema);
