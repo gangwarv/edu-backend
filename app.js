5dd15c6558195c2916952fe28fb9dc8c1bb21e14
Vishal Gangwar <vikash4654@gmail.com>
2020-04-14 22:47:07 +0530

    treeshaked

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-var mongoose = require('mongoose');
-var express = require('express');
-var path = require('path');
-var cors = require('cors')
-var graphqlMiddleware = require('./graphql');
-var authMiddleware = require('./middleware/auth.middleware');
+var mongoose = require("mongoose");
+var express = require("express");
+var path = require("path");
+var cors = require("cors");
+var graphqlMiddleware = require("./graphql");
+var authMiddleware = require("./middleware/auth.middleware");
 
-var { MONGODB_URL, TEST } = require('./keys');
+var { MONGODB_URL } = require("./keys");
 
 var app = express();
 
-app.use(cors())
-// app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/graphql', authMiddleware, graphqlMiddleware);
+app.use("/graphql", authMiddleware, graphqlMiddleware);
 
-app.get('/', function(req, res, next){
-  res.send('Express App! '+ TEST)
+app.get("/", function(req, res, next) {
+  res.send("Express App! " + MONGODB_URL);
 });
 
-// mongoose.connect('mongodb+srv://vishal:vishal@studentcluster-k7i07.mongodb.net/eduerp?retryWrites=true&w=majority', 
-mongoose.connect(MONGODB_URL, 
-{ 
-  useNewUrlParser: true,
-  //useCreateIndex:true 
+// 'mongodb+srv://vishal:vishal@studentcluster-k7i07.mongodb.net/eduerp?retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true
+  //useCreateIndex:true
 });
 
 module.exports = app;
