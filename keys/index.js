f281a5833ba6085bf7caea0aaa4445cf2fed00aa
Vishal Gangwar <vikash4654@gmail.com>
2020-04-29 17:26:18 +0530

    little change

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN_EXPIRY: 30, // in seconds
+  TOKEN_EXPIRY: 300, // in seconds
   MONGODB_URL:
     process.env.DB_CONN ||  //'mongodb+srv://vishal:vishal@studentcluster-k7i07.mongodb.net/eduerp?retryWrites=true&w=majority'
     'mongodb://localhost:27017/eduerp?retryWrites=true' || "mongodb://eduerp-self:passw0rd@52.66.206.37:27017/eduerp?retryWrites=true&useUnifiedTopology=true",
